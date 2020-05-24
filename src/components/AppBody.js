03d8ad9e74e38755c79fe4ddd38dc1ee020162c4
singh-anshuman <anshumansingh137@gmail.com>
2020-05-25 00:07:03 +0530

    Removed unnecessary files added by the create-react-app utility and added react-bootstrap as a dependency to the project

---
+++ src/components/AppBody.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BookCard from './BookCard.js';
+
+export default function AppBody() {
+    return (
+        <div>
+            <BookCard />
+        </div>
+    )
+}
