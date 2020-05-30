ddc6266545c0c23728fce6894d108cbf41d20a29
singh-anshuman <anshumansingh137@gmail.com>
2020-05-30 13:41:58 +0530

    Added redux-devtools in the application for monitoring state and actions

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import bookReducer from "./components/dashboard/reducers/bookReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(bookReducer);
+export const store = createStore(bookReducer, composeWithDevTools());
