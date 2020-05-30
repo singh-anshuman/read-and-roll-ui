8f5befa9c1753c06d02f36aa2f1a992557de67a9
singh-anshuman <anshumansingh137@gmail.com>
2020-05-30 19:41:48 +0530

    Added combine reducer to the store to slice up the state

--- src/components/dashboard/BookList.js
+++ src/components/dashboard/BookList.js
@@ -1,12 +1,11 @@
 import React from "react";
 import BookCard from "./BookCard";
-import { getBooks } from "../../services/BookService";
 import "./css/BookList.css";
 
 export default function BookList(props) {
     return (
         <div className="book-list">
-            {getBooks(props.searchString).map((book) => {
+            {props.books.map((book) => {
                 return <BookCard key={book.id} book={book} />;
             })}
         </div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import bookReducer from "./components/dashboard/reducers/bookReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(bookReducer, composeWithDevTools());
+export const store = createStore(
+    combineReducers({
+        books: bookReducer,
+    }),
+    composeWithDevTools()
+);
