573875372b97af8c0798031788a4e7a6ddfa681d
singh-anshuman <anshumansingh137@gmail.com>
2020-06-21 19:29:46 +0530

    Added reduxImmutableStateInvariant middleware

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { combineReducers, compose, createStore, applyMiddleware } from "redux";
 import referenceDataReducer from "./components/common/reducers/referenceDataReducer";
 import bookReducer from "./components/dashboard/reducers/bookReducer";
 import thunk from 'redux-thunk';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -9,5 +10,5 @@ export const store = createStore(
     combineReducers({
         books: bookReducer,
         referenceData: referenceDataReducer,
-    },composeEnhancers(applyMiddleware(thunk)))
+    },composeEnhancers(applyMiddleware(reduxImmutableStateInvariant,thunk)))
 );
