81d17bea4489fadcafec57589963f48c1362101e
singh-anshuman <anshumansingh137@gmail.com>
2020-06-22 10:02:27 +0530

    Reverted code for adding thunk middleware to the application

--- src/store.js
+++ src/store.js
@@ -2,14 +2,13 @@ import { combineReducers, compose, createStore, applyMiddleware } from "redux";
 import referenceDataReducer from "./components/common/reducers/referenceDataReducer";
 import bookReducer from "./components/dashboard/reducers/bookReducer";
 import thunk from 'redux-thunk';
-//  import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
-//  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     combineReducers({
         books: bookReducer,
         referenceData: referenceDataReducer,
-    },applyMiddleware(thunk))
-    //},composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())))
+    },composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())))
 );
