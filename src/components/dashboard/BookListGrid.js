af8e4d694098753cd0cb80bea9274e8fb6757ddc
singh-anshuman <anshumansingh137@gmail.com>
2020-06-24 09:37:42 +0530

    Added ag-grid to the BookListGrid component

--- src/components/dashboard/BookListGrid.js
+++ src/components/dashboard/BookListGrid.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 import * as bookActions from './actions/bookActions';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 
 
 class BookListGrid extends React.Component {
+
     componentDidMount() {
         this.props.actions.fetchBooks();
     }
 
     render() {
+        const columnDefs = [
+            { headerName: "Title", field: "title" },
+            { headerName: "Author", field: "author" },
+            { headerName: "Category", field: "category" }
+        ];
         return (
-            <div>
-                Book List Grid
+            <div className="ag-theme-alpine" style={ {height: '200px', width: '600px'} }>
+                <AgGridReact
+                    columnDefs={columnDefs}
+                    rowData={this.props.books.data}>
+                </AgGridReact>
             </div>
         );
     }
