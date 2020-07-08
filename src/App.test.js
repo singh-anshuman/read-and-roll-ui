4b071a948a98874c995f6761156586a779aa9b58
singh-anshuman <anshumansingh137@gmail.com>
2020-07-08 09:48:15 +0530

    Added a dummy test for checking jest mocking capability

--- src/App.test.js
+++ src/App.test.js
@@ -10,4 +10,11 @@ import App from './App';
 
 test('First JEST test',()=> {
   expect(true).toBeTruthy();
-})
+});
+
+const testFn = jest.fn(()=>{return 23;});
+
+test('First test with jest mocking',() => {
+  testFn();
+  expect(testFn).toBeCalledTimes(1);     
+});
